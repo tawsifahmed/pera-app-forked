53faa5267cd7b6044f5897ba74abbe7a1a841715
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-05-02 17:08:09 +0600

    code update

--- pages/index.vue
+++ pages/index.vue
@@ -62,11 +62,7 @@ const checkUser = () => {
 
 onMounted(() => {
     checkUser()
-<<<<<<< HEAD
-    
-=======
     // ProductService.getProductsSmall().then((data) => (products.value = data));
->>>>>>> c1716a978e4ec8af78e165d8c88498a768269fb5
 });
 
 const formatCurrency = (value) => {
