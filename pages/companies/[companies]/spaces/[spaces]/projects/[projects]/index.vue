1dbbd9f76d7153206c79b0751a6672338e363156
Rafid Al Ridwan <rafid.singularity@gmail.com>
2024-05-06 15:10:22 +0600

    update

--- pages/companies/[companies]/spaces/[spaces]/projects/[projects]/index.vue
+++ pages/companies/[companies]/spaces/[spaces]/projects/[projects]/index.vue
@@ -69,21 +69,22 @@
     </div>
 </template>
 <script setup>
-import { storeToRefs } from 'pinia';
-import Dialog from 'primevue/dialog';
-import { useCompanyStore } from '~/store/company';
-const { getSingleProject, editTask, deleteTask, getTaskAssignModalData, addTaskComment } = useCompanyStore();
-const { singleProject, isTaskDeleted, isTaskEdited, tasks, isTaskCommentCreated } = storeToRefs(useCompanyStore());
-
-const usersListStore = useCompanyStore();
 // Access users data
 definePageMeta({
     middleware: 'auth',
     layout: 'default'
 });
 
+import { storeToRefs } from 'pinia';
 import { FilterMatchMode } from 'primevue/api';
 import Column from 'primevue/column';
+import Dialog from 'primevue/dialog';
+import { useCompanyStore } from '~/store/company';
+
+const usersListStore = useCompanyStore();
+const { getSingleProject, editTask, deleteTask, getTaskAssignModalData, addTaskComment, getTaskDetails } = useCompanyStore();
+const { singleProject, isTaskDeleted, isTaskEdited, tasks, isTaskCommentCreated } = storeToRefs(useCompanyStore());
+
 const filters = ref({});
 const loading = ref(true);
 const toast = useToast();
@@ -97,22 +98,6 @@ const refTaskId = ref(null);
 const taskId = ref(null);
 const createTaskTitle = ref(null);
 
-const openCreateSpace = (key, type) => {
-    if (key) {
-        taskId.value = key;
-    } else {
-        taskId.value = '';
-    }
-    console.log('taskId', taskId.value);
-    if (type == 'sub-task') {
-        createTaskTitle.value = 'Create Sub Task';
-    } else {
-        createTaskTitle.value = 'Create Task';
-    }
-    visible.value = true;
-    console.log('visible', visible.value);
-};
-
 const taskNameEditInput = ref(null);
 const taskEditDescriptionInput = ref(null);
 const priority = ref(null);
@@ -121,10 +106,25 @@ const assignees = ref([]);
 const refTaskIdForEdit = ref(null);
 const usersLists = ref([]);
 const visibleEdit = ref(false);
+const deleteTaskDialog = ref(false);
+const visibleTaskDetailView = ref(false);
+
+const openCreateSpace = (key, type) => {
+    if (key) {
+        taskId.value = key;
+    } else {
+        taskId.value = '';
+    }
+    if (type == 'sub-task') {
+        createTaskTitle.value = 'Create Sub Task';
+    } else {
+        createTaskTitle.value = 'Create Task';
+    }
+    visible.value = true;
+};
 
 const handleTaskEdit = async (task) => {
     singleTask.value = task;
-    // console.log(task);
 
     await getTaskAssignModalData(); // Await the function call
     usersLists.value = usersListStore.users;
@@ -143,7 +143,6 @@ const confirmDeleteTask = (taskId) => {
     deleteTaskDialog.value = true;
 };
 
-const deleteTaskDialog = ref(false);
 
 const deletingTask = async () => {
     btnLoading.value = true;
@@ -158,13 +157,11 @@ const deletingTask = async () => {
     }
 };
 
-const visibleTaskDetailView = ref(false);
 const handleTaskDetailView = (task) => {
-    console.log('task', task);
     singleTask.value = task;
     refTaskId.value = task.key;
     taskNameEditInput.value = task.data.name;
-    // getSingleTaskComments(task.key);
+    // getTaskDetails(task.key);
     visibleTaskDetailView.value = true;
 };
 
