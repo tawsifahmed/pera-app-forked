ba1e792e4495029b76bb34cf7014ea6ae6bfff3b
AR <rafi.singularity@gmail.com>
2024-09-01 16:53:12 +0600

    all task view added to dashboard

--- pages/kpi-reports/index.vue
+++ pages/kpi-reports/index.vue
@@ -17,18 +17,6 @@ const pmMark = ref();
 const teamBuilding = ref();
 const hrMark = ref();
 
-// Date Formatter
-const dateFormatter = (data) => {
-    const dateStr = data;
-    const date = new Date(dateStr);
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() returns 0-11
-    const day = String(date.getDate()).padStart(2, '0');
-
-    return `${year}-${month}-${day}`;
-};
-
 const handleReportDownload = async (e) => {
     e.preventDefault();
     // Validation
