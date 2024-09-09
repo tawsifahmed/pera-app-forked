a6cb7d84d6f2f4f1756de56c348c7fd52269f91a
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-09-09 10:40:28 +0600

    roles page route update

--- pages/index.vue
+++ pages/index.vue
@@ -353,7 +353,7 @@ watch(
         </div>
         <div class="col-12 lg:col-6 xl:col-3">
             <div class="card mb-0">
-                <NuxtLink v-if="readRole" to="/role" class="flex justify-content-between mb-3">
+                <NuxtLink v-if="readRole" to="/roles" class="flex justify-content-between mb-3">
                     <div>
                         <span class="block text-500 font-medium mb-3">Roles</span>
                         <div class="text-900 font-medium text-xl">{{ rolesLists ? rolesLists.length : '0' }}</div>
