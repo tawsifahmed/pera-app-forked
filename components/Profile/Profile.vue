34a09f2317b590a8b8399dbdd6d16c032a486f3e
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-09-23 16:38:00 +0600

    profile section UI updated

--- components/Profile/Profile.vue
+++ components/Profile/Profile.vue
@@ -1,6 +1,8 @@
 <script setup>
 // const profileData = ref(userProfile);
 import { useUserStore } from '~/store/user';
+import Password from 'primevue/password';
+
 
 const { userProfile } = defineProps(['userProfile']);
 const { updateUser } = useUserStore();
@@ -13,6 +15,8 @@ const uploadedImage = ref(null);
 const phone = ref(userProfile?.data?.phone);
 const email = ref(userProfile?.data?.email);
 const address = ref(userProfile?.data?.address);
+const newPassword = ref(null);
+const confirmPassword = ref(null);
 
 // Form submission
 
@@ -34,47 +38,76 @@ const handleImageUpload = (value) => {
 </script>
 <template>
     <!-- <pre>{{ userProfile }}</pre> -->
-    <form @submit.prevent="handleSubmit" class="grid">
-        <div class="col-12 text-center mb-5">
-            <div class="relative w-fit mx-auto">
-                <img v-if="imageData" :src="`${imageData}`" style="height: 100px; width: 100px; border-radius: 100%; object-fit: cover" />
-                <img v-else src='../../assets/dummy_profile.png' alt="" style="height: 100px; width: 100px; border-radius: 100%; object-fit: cover">
-                <div class="img-label">
-                    <label for="image">
-                        <i class="pi pi-plus" style="color: red; right: 0.2rem; bottom: 0.2rem; z-index: 5; background-color: white; padding: 8px; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; cursor: pointer"></i>
-                    </label>
-                    <input class="hidden" type="file" :v-model="uploadedImage" id="image" @input="(event) => handleImageUpload(event)" accept=".png, .jpeg, .jpg" />
+    <TabView class="mt-3">
+        <TabPanel  header="General Info">
+            <form @submit.prevent="handleSubmit" class="grid">
+                <div class="col-12 text-center mb-5">
+                    <div class="relative w-fit mx-auto">
+                        <img v-if="imageData" :src="`${imageData}`" style="height: 100px; width: 100px; border-radius: 100%; object-fit: cover" />
+                        <img v-else src='../../assets/dummy_profile.png' alt="" style="height: 100px; width: 100px; border-radius: 100%; object-fit: cover">
+                        <div class="img-label">
+                            <label for="image">
+                                <i class="pi pi-plus" style="color: red; right: 0.2rem; bottom: 0.2rem; z-index: 5; background-color: white; padding: 8px; border-radius: 20px; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; cursor: pointer"></i>
+                            </label>
+                            <input class="hidden" type="file" :v-model="uploadedImage" id="image" @input="(event) => handleImageUpload(event)" accept=".png, .jpeg, .jpg" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-12 lg:col-6 mb-3">
-            <FloatLabel>
-                <InputText id="name" class="w-full" v-model="userName" />
-                <label for="name">Name</label>
-            </FloatLabel>
-        </div>
-        <div class="col-12 lg:col-6 mb-3">
-            <FloatLabel>
-                <InputText id="phone" v-model="phone" type="number" class="w-full" />
-                <label for="phone">Phone</label>
-            </FloatLabel>
-        </div>
-        <div class="col-12 lg:col-6 mb-3">
-            <FloatLabel>
-                <InputText id="email" v-model="email" type="email" class="w-full" disabled />
-                <label for="email">Email</label>
-            </FloatLabel>
-        </div>
-        <div class="col-12 lg:col-6 mb-3">
-            <FloatLabel>
-                <InputText id="address" v-model="address" class="w-full" />
-                <label for="address">Address</label>
-            </FloatLabel>
-        </div>
-        <div class="col-12 mb-3 flex justify-content-center">
-            <Button type="submit" label="Save" :loading="loading" />
-        </div>
-    </form>
+                <div class="col-12 lg:col-6 mb-3">
+                    <FloatLabel>
+                        <InputText id="name" class="w-full" v-model="userName" />
+                        <label for="name">Name</label>
+                    </FloatLabel>
+                </div>
+                <div class="col-12 lg:col-6 mb-3">
+                    <FloatLabel>
+                        <InputText id="phone" v-model="phone" type="number" class="w-full" />
+                        <label for="phone">Phone</label>
+                    </FloatLabel>
+                </div>
+                <div class="col-12 lg:col-6 mb-3">
+                    <FloatLabel>
+                        <InputText id="email" v-model="email" type="email" class="w-full" disabled />
+                        <label for="email">Email</label>
+                    </FloatLabel>
+                </div>
+                <div class="col-12 lg:col-6 mb-3">
+                    <FloatLabel>
+                        <InputText id="address" v-model="address" class="w-full" />
+                        <label for="address">Address</label>
+                    </FloatLabel>
+                </div>
+                <div class="col-12 mb-3 flex justify-content-center">
+                    <Button type="submit" label="Save" :loading="loading" />
+                </div>
+            </form>
+        </TabPanel>
+        <TabPanel header="Password">
+            <br>
+            <br>
+
+            <form @submit.prevent="handleSubmit" class="grid form-width mx-auto">
+                <div class="col-12 mb-3">
+                    <FloatLabel>
+                        <Password id="passw" v-model="newPassword" placeholder="Enter new password" :feedback="false" toggleMask class="w-full" inputClass="w-full" :inputStyle="{ padding: '0.78rem' }"></Password>
+
+                        <label for="passw">New Password</label>
+                    </FloatLabel>
+                </div>
+                <div class="col-12 ">
+                    <FloatLabel>
+                        <Password id="password2" v-model="confirmPassword" placeholder="Confirm new password" :feedback="false" toggleMask class="w-full" inputClass="w-full" :inputStyle="{ padding: '0.78rem' }"></Password>
+
+                        <label for="password2">Confirm Password</label>
+                    </FloatLabel>
+                </div>
+                <div class="col-12 mt-3 mb-3 px-5 flex justify-content-center">
+                    <Button type="submit" label="Save" :loading="loading" />
+                </div>
+            </form>
+        </TabPanel>
+    </TabView>
+
 </template>
 
 <style lang="scss" scoped>
@@ -104,5 +137,9 @@ input::-webkit-inner-spin-button {
 input[type=number] {
     appearance: textfield;
     -moz-appearance: textfield; 
+}
+
+.form-width{
+    width: 50%;
 }
 </style>
