c2baefb0f6d50c10543151d716693630e0a54339
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2024-11-04 12:35:45 +0600

    user reports download funct. and UI updated

--- pages/user-reports/index.vue
+++ pages/user-reports/index.vue
@@ -13,8 +13,8 @@ definePageMeta({
 const startDate = ref('');
 
 const selectedDateCategory = ref('Create');
-const selectedDateCategory2 = ref('Create');
-console.log('Selected Date Category', selectedDateCategory.value);
+// const selectedDateCategory2 = ref('Create');
+
 const dateCategories = ref([
     { name: 'Create', key: 'A' },
     { name: 'Due Date', key: 'B' },
@@ -45,29 +45,55 @@ const dateFormatter = (data) => {
 const handleGenerate = async () => {
     if (employee.value == '') {
         return toast.add({ severity: 'warn', summary: 'Warning', detail: 'Please Select User First', group: 'br', life: 3000 });
-    } else if (startDate.value == '' || endDate.value == '') {
-        return toast.add({ severity: 'warn', summary: 'Warning', detail: 'Please Select Start Date and End Date', group: 'br', life: 3000 });
+    }
+    if ((startDate.value && !endDate.value) || (!startDate.value && endDate.value)) {
+        loading.value = false;
+        return toast.add({ severity: 'warn', summary: 'Warning', detail: 'Please Select Both Dates', group: 'br', life: 3000 });
     }
     const token = useCookie('token');
     loading.value = true;
     const formattedStartDate = dateFormatter(startDate.value);
     const formattedEndDate = dateFormatter(endDate.value);
+    const formData = new FormData();
+    const userIds = employee.value?.map(item => item.id);
+    console.log('User ID', userIds);
+    formData.append('user_id[]', userIds);
 
-    const { data, error } = await useFetch(`${url.public.apiUrl}/tasks/task-report-view?start_date=${startDate.value}&end_date=${endDate.value}&user_id=${employee.value.id}`, {
-        method: 'GET',
+    // Only append project IDs if there are valid projects selected
+    if (selectedProject.value && selectedProject.value.length > 0) {
+        const projectIds = selectedProject.value?.map(item => item.id);
+        console.log('Project ID', projectIds);
+        if (projectIds.length > 0) {
+            formData.append('project_id[]', projectIds);
+        }
+    }
+
+    if (startDate.value && endDate.value) {
+        if (selectedDateCategory.value == 'Create') {
+            formData.append('start_date', formattedStartDate);
+            formData.append('end_date', formattedEndDate);
+        } else {
+            formData.append('start_due_date', formattedStartDate);
+            formData.append('end_due_date', formattedEndDate);
+        }
+    }
+
+    const { data, error } = await useFetch(`${url.public.apiUrl}/tasks/task-report-view`, {
+        method: 'POST',
         headers: {
             Authorization: `Bearer ${token.value}`
-        }
+        },
+        body: formData
     });
-    if (data.value.code == 200) {
+
+    if (data.value?.code == 200) {
         previewData.value = data.value.data;
         return (loading.value = false);
     } else {
         loading.value = false;
-        return toast.add({ severity: 'error', summary: 'Failed', detail: 'Failed to download', group: 'br', life: 3000 });
+        return toast.add({ severity: 'error', summary: 'Failed', detail: 'Failed to generate!', group: 'br', life: 3000 });
     }
 };
-
 const handleReportDownload = async () => {
     const token = useCookie('token');
     loading1.value = true;
@@ -75,19 +101,44 @@ const handleReportDownload = async () => {
         loading1.value = false;
         return toast.add({ severity: 'error', summary: 'Failed', detail: 'Please Select Employee', group: 'br', life: 3000 });
     }
-    if (startDate.value == '' && endDate.value !== '') {
-        loading1.value = false;
-        return toast.add({ severity: 'error', summary: 'Failed', detail: 'Please Select Start Date', group: 'br', life: 3000 });
+    if ((startDate.value && !endDate.value) || (!startDate.value && endDate.value)) {
+        loading.value = false;
+        return toast.add({ severity: 'warn', summary: 'Warning', detail: 'Please Select Both Dates', group: 'br', life: 3000 });
     }
     const formattedStartDate = dateFormatter(startDate.value);
     const formattedEndDate = dateFormatter(endDate.value);
+    const formData = new FormData();
+    const userIds = employee.value?.map(item => item.id);
+    console.log('User ID', userIds);
+    // return
+    formData.append('user_id[]', userIds);
+    console.log('User ID', formData)
+    if (selectedProject.value && selectedProject.value.length > 0) {
+        const projectIds = selectedProject.value?.map(item => item.id);
+        console.log('Project ID', projectIds);
+        if (projectIds.length > 0) {
+            formData.append('project_id[]', projectIds);
+        }
+    }
+    
+    if (startDate.value && endDate.value) {
+        if (selectedDateCategory.value == 'Create') {
+            formData.append('start_date', formattedStartDate);
+            formData.append('end_date', formattedEndDate);
+        } else {
+            formData.append('start_due_date', formattedStartDate);
+            formData.append('end_due_date', formattedEndDate);
+        }
+    }
 
     const { data, error } = await useFetch(
-        `${url.public.apiUrl}/tasks/task-report-download?user_id=${employee.value.id}${startDate.value !== '' ? `&start_date=${formattedStartDate}` : ''}${endDate.value !== '' ? `&end_date=${formattedEndDate}` : ''}`,
+        `${url.public.apiUrl}/tasks/task-report-download`,
         {
+            method: 'POST',
             headers: {
                 Authorization: `Bearer ${token.value}`
-            }
+            },
+            body: formData
         }
     );
     if (error.value) {
@@ -138,6 +189,10 @@ onMounted(() => {
         <Toast position="bottom-right" group="br" />
         <div class="d-flex mr-2">
             <h5 class="mb-1">User Reports</h5>
+            <pre>
+                sp =>{{  selectedProject?.length }}
+            
+            </pre>
         </div>
         <Toolbar class="border-0 px-0">
             <template #start>
@@ -145,40 +200,36 @@ onMounted(() => {
                     <div class="user-selection w-full md:w-14rem w-full">
                         <label class="font-bold block mb-2">User:</label>
                         <div class="flex justify-content-center">
-                            <MultiSelect display="chip" v-model="employee" :options="employees" filter optionLabel="name"
-                placeholder="Select User" class="w-full" />
-                            </div>
+                            <MultiSelect display="chip" v-model="employee" :options="employees" filter
+                                optionLabel="name" placeholder="Select User" class="w-full" />
+                        </div>
                     </div>
                     <div class="user-selection w-full md:w-14rem w-full">
                         <label class="font-bold block mb-2">Project:</label>
                         <div class="flex justify-content-center">
-                            <MultiSelect display="chip" v-model="selectedProject" :options="totalProjects" filter optionLabel="name"
-                placeholder="Select Project" class="w-full" />
-                            </div>
+                            <MultiSelect display="chip" v-model="selectedProject" :options="totalProjects" filter
+                                optionLabel="name" placeholder="Select Project" class="w-full" />
+                        </div>
                     </div>
                     <div class="flex-auto">
-                        <div class="flex gap-3 align-items-center">
-                            <label for="icondisplay" class="font-bold block mb-2">From: </label>
-                            <div class="flex flex-wrap gap-2 mb-2 text-sm">
-                                <div v-for="category in dateCategories" :key="category.key" class="flex align-items-center">
-                                    <RadioButton v-model="selectedDateCategory" :inputId="category.key" name="dynamic" :value="category.name" />
-                                    <label :for="category.key" class="ml-2">{{ category.name }}</label>
-                                </div>
-                            </div>
-                        </div>
-                        <Calendar v-model="startDate" @date-select="handleChange('startDate', $event)" showIcon iconDisplay="input" inputId="icondisplay" />
+                        <label for="icondisplay" class="font-bold block mb-2">From: </label>
+                        <Calendar v-model="startDate" @date-select="handleChange('startDate', $event)" showIcon
+                            iconDisplay="input" inputId="icondisplay" />
                     </div>
                     <div class="flex-auto">
-                        <div class="flex gap-3 align-items-center">
-                            <label for="icondisplay" class="font-bold block mb-2">To: </label>
-                            <div class="flex flex-wrap gap-2 mb-2 text-sm">
-                                <div v-for="category in dateCategories" :key="category.key" class="flex align-items-center">
-                                    <RadioButton v-model="selectedDateCategory2" :inputId="category.key" name="dynamic" :value="category.name" />
-                                    <label :for="category.key" class="ml-2">{{ category.name }}</label>
-                                </div>
+                        <label for="icondisplay" class="font-bold block mb-2">To: </label>
+                        <Calendar v-model="endDate" @date-select="handleChange('endtDate', $event)" showIcon
+                            iconDisplay="input" inputId="icondisplay" />
+                    </div>
+                    <div class="flex-auto ml-2">
+                        <label for="icondisplay" class="font-bold block mb-2">Date Type: </label>
+                        <div class="flex flex-wrap gap-2 mt-3 mb-2 text-sm">
+                            <div v-for="category in dateCategories" :key="category.key" class="flex align-items-center">
+                                <RadioButton v-model="selectedDateCategory" :inputId="category.key" name="dynamic"
+                                    :value="category.name" />
+                                <label :for="category.key" class="ml-2">{{ category.name }}</label>
                             </div>
                         </div>
-                        <Calendar v-model="endDate" @date-select="handleChange('endtDate', $event)" showIcon iconDisplay="input" inputId="icondisplay" />
                     </div>
                 </div>
             </template>
@@ -195,7 +246,9 @@ onMounted(() => {
                 <Button @click="handleReportDownload" class="w-fit" label="Download" :loading="loading1" />
             </div>
             <DataTable :value="previewData" tableStyle="min-width: 50rem">
-                <template #empty> <p class="text-center">No Data found...</p> </template>
+                <template #empty>
+                    <p class="text-center">No Data found...</p>
+                </template>
                 <Column field="task_name" header="Task"></Column>
                 <Column field="project_name" header="Project"></Column>
                 <Column field="assignee_name" header="Assignee"></Column>
