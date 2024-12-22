b13070ed47ea5a89810262276dda97366b4847a8
Ar Rafi Uddin <rafi.singularity@gmail.com>
2024-12-22 23:26:25 +0600

    Meeting Minutes complete

--- components/Scrum/ScrumTable.vue
+++ components/Scrum/ScrumTable.vue
@@ -1,18 +1,27 @@
 <script setup>
 const { scrumData } = defineProps(['scrumData']);
+const scrumModal = ref(false);
+const selectedScrum = ref(null);
+
+const handleRowClick = (data) => {
+    selectedScrum.value = data;
+    scrumModal.value = true;
+};
 </script>
 <template>
-    <DataTable :value="scrumData" v-if="scrumData && scrumData.length > 0" stripedRows tableStyle="min-width: 50rem">
+    <DataTable class="table-st" :value="scrumData" :rowHover="true" @row-click="handleRowClick" v-if="scrumData && scrumData.length > 0" stripedRows tableStyle="min-width: 50rem">
         <Column style="width: 2%" field="id" header="id"></Column>
         <Column style="width: 10%" field="meeting_date" header="Date"></Column>
         <Column style="width: 10%" field="space_id" header="Space">
             <template #body="slotProps">
-                <div>
-                    {{ slotProps.data.space_id.name }}
-                </div>
+                <div>{{ slotProps.data.space_id.name }}</div>
             </template>
         </Column>
-        <Column style="width: 30%" field="description" header="Description"></Column>
+        <!-- <Column style="width: 30%" field="description" header="Description">
+            <template #body="slotProps">
+                <div class="description" v-html="slotProps.data.description"></div>
+            </template>
+        </Column> -->
         <Column style="width: 38%" field="user ids" header="Participants">
             <template #body="slotProps">
                 <!-- <pre>{{ slotProps }}</pre> -->
@@ -24,6 +33,21 @@ const { scrumData } = defineProps(['scrumData']);
             </template>
         </Column>
     </DataTable>
+
+    <Dialog lazy="true" v-model:visible="scrumModal" modal :header="selectedScrum?.data?.space_id?.name" :style="{ minWidth: '35vw' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+        <div class="title">
+            <p class="">Participants:</p>
+            <p class="">{{ selectedScrum.data.meeting_date }}</p>
+        </div>
+        <div class="perticipants">
+            <div class="flex gap-1">
+                <div class="border rounded" v-for="child in selectedScrum.data.user_ids" :key="child.id" style="border: 1px solid rgba(167, 167, 167, 0.486); border-radius: 5px; padding: 2px 5px">
+                    <div class="">{{ child.name }}</div>
+                </div>
+            </div>
+        </div>
+        <div class="details" v-html="selectedScrum?.data.description"></div>
+    </Dialog>
 </template>
 
 <style scoped>
@@ -33,5 +57,43 @@ const { scrumData } = defineProps(['scrumData']);
 }
 .description {
     width: 60%;
+    display: block; /* Ensures the element behaves like a block for truncation */
+    overflow: hidden; /* Hides overflowing content */
+    white-space: nowrap; /* Prevents text wrapping */
+    text-overflow: ellipsis; /* Adds "..." to indicate truncated content */
+    line-height: 1.5; /* Adjusts the line height for better readability */
+}
+.table-st {
+    border: 1px solid #ededed;
+    border-radius: 10px;
+    overflow: hidden;
+}
+.table-st thead tr {
+    background: #ededed;
+}
+.title {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem;
+    border-bottom: 1px solid #ededed;
+    font-size: 14px;
+    /* font-weight: 500; */
+}
+.title p {
+    margin: 0;
+}
+.perticipants {
+    margin: 8px 0;
+    display: flex;
+    gap: 10px;
+    flex-wrap: wrap;
+}
+.participants p {
+    font-size: 18px;
+}
+.details {
+    padding: 16px;
+    border: 1px solid #ededed;
+    border-radius: 8px;
 }
 </style>
