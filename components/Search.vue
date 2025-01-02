8bff8443d92e4b8edca666bf3cf0a87e7d796161
Tawsif Ahmed Riyad <tawsifahmed@iut-dhaka.edu>
2025-01-02 12:19:57 +0600

    searchbar UI component initalization #1

---
+++ components/Search.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-full" >
+        <div class="flex justify-content-center">
+            <IconField iconPosition="left" style="width: 50%;">
+                <InputIcon class="pi pi-search text-xl"> </InputIcon>
+                <InputText class="w-full" v-model="searchText" placeholder="Search..." size="large"/>
+            </IconField>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<script setup>
+const searchText = ref('');
+</script>
+
+<style lang="scss" scoped>
+
+</style>
