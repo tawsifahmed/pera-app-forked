93f88307d02aa6e60525087d2b282aa25f6ae1d8
AR <rafi@singularitybd.com>
2025-01-13 16:27:41 +0600

    attendance added

---
+++ components/Attendance/AttendanceHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    createModal: Boolean
+});
+</script>
+<template>
+    <header class="header">
+        <h3>Attendance</h3>
+    </header>
+</template>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    /* background-color: #f8f9fa; */
+}
+</style>
